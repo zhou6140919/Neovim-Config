54b53f206b25de19b7f3d359942fae0151efae86
Tong Zhou <tongzhou@vt.edu>
2023-09-13 14:46:22 -0400

    mac version

---
+++ init.lua
@@ -0,0 +1,34 @@
+-- 基础设置
+require("basic")
+-- 快捷键映射
+require("keybindings")
+-- Packer 插件管理
+require("plugins")
+-- 主题设置
+require("colorscheme")
+-- 插件配置
+require("plugin-config.nvim-tree")
+require("plugin-config.bufferline")
+require("plugin-config.lualine")
+require("plugin-config.telescope")
+require("plugin-config.dashboard")
+require("plugin-config.project")
+require("plugin-config.nvim-treesitter")
+require("plugin-config.indent-blankline")
+require("plugin-config.colorizer")
+require("plugin-config.neoscroll")
+-- require("plugin-config.transparent")
+-- require("copilot")
+-- require("copilot-cmp")
+-- 内置LSP
+require("lsp.setup")
+require("lsp.ui")
+require("lsp.null-ls")
+require("lsp.cmp")
+
+vim.cmd([[imap <silent><script><expr> <C-y> copilot#Accept("\<CR>")]])
+vim.g.copilot_no_tab_map = true
+vim.cmd([[highlight CopilotSuggestion guifg=#555555 ctermfg=8]])
+vim.cmd([[set updatetime=100]])
+
+vim.cmd([[set mouse=]])
---
+++ lua/lsp/null-ls.lua
@@ -0,0 +1,60 @@
+local status, null_ls = pcall(require, "null-ls")
+if not status then
+	vim.notify("没有找到 null-ls")
+	return
+end
+
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+
+null_ls.setup({
+	debug = false,
+	sources = {
+		-- Diagnostics
+		-- diagnostics.flake8,
+		diagnostics.pylint,
+		-- Formatting ---------------------
+		--  brew install shfmt
+		formatting.shfmt,
+		-- StyLua
+        -- formatting.stylua,
+        -- formatting.lua_format,
+		-- python
+		formatting.autopep8,
+		-- frontend
+		formatting.prettier.with({ -- 只比默认配置少了 markdown
+			filetypes = {
+				"javascript",
+				"javascriptreact",
+				"typescript",
+				"typescriptreact",
+				"vue",
+				"css",
+				"scss",
+				"less",
+				"html",
+				"json",
+				"yaml",
+				"graphql",
+			},
+			prefer_local = "node_modules/.bin",
+		}),
+		-- formatting.fixjson,
+		-- formatting.black.with({ extra_args = { "--fast" } }),
+	},
+	-- 保存自动格式化
+	on_attach = function(client)
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = false })
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_create_autocmd("BufWritePre", {
+			    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr }),
+				group = augroup,
+				buffer = bufnr,
+				-- on 0.8, you should use vim.lsp.buf.format instead
+				callback = function()
+                    vim.lsp.buf.format()
+                end,
+			})
+		end
+	end,
+})
