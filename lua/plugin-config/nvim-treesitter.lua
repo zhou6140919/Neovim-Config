42dafd5487612547a28347c91e5271f61240eb9f
Tong Zhou <tongzhou@vt.edu>
2023-09-13 14:58:13 -0400

    fix tree-sitter bugs

--- lua/plugin-config/nvim-treesitter.lua
+++ lua/plugin-config/nvim-treesitter.lua
@@ -7,7 +7,7 @@ end
 treesitter.setup({
 	-- 安装 language parser
 	-- :TSInstallInfo 命令查看支持的语言
-	ensure_installed = { "json", "html", "css", "vim", "lua", "javascript", "typescript", "tsx" },
+	ensure_installed = { "json", "html", "css", "vim", "javascript", "typescript", "tsx" },
 	-- 启用代码高亮模块
 	highlight = {
 		enable = true,
